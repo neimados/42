0ae81477f5d21ca5fbcfa1048889b16205cfe2d1
dso <so.damien@gmail.com>
2021-12-16 22:59:01 +0100

    starting pipex

---
+++ pipex/sources/ft_split.c
@@ -0,0 +1,106 @@
+#include "../includes/pipex.h"
+
+void	freemalloc(char **strs)
+{
+	int	i;
+
+	i = 0;
+	while (strs[i])
+	{
+		free(strs[i]);
+		i++;
+	}
+	free(strs);
+	return ;
+}
+
+static int	ft_countwords(char const *str, char c)
+{
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != c && str[i])
+		{
+			count++;
+			while (str[i] != c && str[i])
+				i++;
+		}
+		else
+			i++;
+	}
+	return (count);
+}
+
+static void	ft_malloctab(char const *str, char c, char **strs)
+{	
+	int	i;
+	int	j;
+	int	count;
+
+	i = 0;
+	j = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			i++;
+		else
+		{
+			while (str[i] != c && str[i])
+			{
+				count++;
+				i++;
+			}
+			strs[j] = malloc(sizeof(char) * (count + 1));
+			if (!strs[j])
+				return (freemalloc(strs));
+			j++;
+		}
+	}
+}
+
+static void	ft_filltab(char const *str, char c, char **strs, int words)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (str[i])
+	{
+		if (str[i] == c && str[i])
+			i++;
+		if (str[i] != c && str[i])
+		{
+			while (str[i] != c && str[i])
+				strs[j][k++] = str[i++];
+			strs[j][k] = '\0';
+			k = 0;
+			if (j < words)
+				j++;
+		}
+	}
+}
+
+char	**ft_split(char *s, char c)
+{
+	char	**strs;
+	int		words;
+
+	if (!s)
+		return (NULL);
+	words = ft_countwords(s, c);
+	strs = malloc(sizeof(char *) * (words + 1));
+	if (!strs)
+		return (NULL);
+	ft_malloctab(s, c, strs);
+	ft_filltab(s, c, strs, words);
+	strs[words] = NULL;
+	return (strs);
+}
---
+++ pipex/sources/pipex_utils.c
@@ -0,0 +1,54 @@
+#include "../includes/pipex.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char	*ptr;
+	size_t	i;
+
+	ptr = malloc(size * count);
+	i = 0;
+	if (!ptr)
+		return (NULL);
+	while (i < size * count)
+	{
+		ptr[i] = '\0';
+		i++;
+	}
+	return (ptr);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	if (!s1)
+		s1 = ft_calloc(1, sizeof(char));
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	j = 0;
+	str = ft_calloc((ft_strlen(s1) + ft_strlen(s2) + 1), sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+		str[i++] = s2[j++];
+	free(s1);
+	return (str);
+}
